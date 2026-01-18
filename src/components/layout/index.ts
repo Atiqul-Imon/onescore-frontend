b878123ecfdc5c81d0e24cb823e692392e62075d
atiqul-islam <imonatikulislam@gmail.com>
2026-01-19 03:22:26 +0600

    refector homepage hero section with live demo and also add mobile bottom navigation bar

--- src/components/layout/index.ts
+++ src/components/layout/index.ts
@@ -2,4 +2,5 @@ export { Header } from './Header';
 export { PageLayout } from './PageLayout';
 export { Footer } from './Footer';
 export { Breadcrumbs } from './Breadcrumbs';
+export { MobileBottomNav } from './MobileBottomNav';
 
