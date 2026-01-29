4381279abddebe9efcfdfd56e574858b4e88e990
atiqul-islam <imonatikulislam@gmail.com>
2026-01-30 00:36:02 +0600

    fix: update ESLint config and lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,10 +7,9 @@ module.exports = {
     'prettier', // Must be last to override other configs
   ],
   ignorePatterns: ['node_modules/', '.next/', 'dist/', 'coverage/', 'playwright-report/'],
-  plugins: ['@typescript-eslint', 'testing-library'],
   rules: {
     // TypeScript rules - gradually enforce
-    '@typescript-eslint/no-explicit-any': 'warn', // Changed from 'off' to 'warn'
+    '@typescript-eslint/no-explicit-any': 'warn',
     '@typescript-eslint/no-unused-vars': [
       'warn',
       {
@@ -19,20 +18,20 @@ module.exports = {
         caughtErrorsIgnorePattern: '^_',
       },
     ],
-    '@typescript-eslint/explicit-function-return-type': 'off', // Too strict for now
+    '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/no-non-null-assertion': 'warn',
     
     // React rules
     'react-hooks/exhaustive-deps': 'warn',
     'react/no-unescaped-entities': 'off',
-    'react/prop-types': 'off', // Using TypeScript for prop validation
+    'react/prop-types': 'off',
     
     // Next.js rules
-    '@next/next/no-img-element': 'warn', // Changed from 'off' to 'warn'
-    '@next/next/no-html-link-for-pages': 'warn', // Changed from 'off' to 'warn'
+    '@next/next/no-img-element': 'warn',
+    '@next/next/no-html-link-for-pages': 'warn',
     
     // Code quality rules
-    'no-console': ['warn', { allow: ['warn', 'error'] }], // Warn on console.log, allow console.warn/error
+    'no-console': ['warn', { allow: ['warn', 'error'] }],
     'prefer-const': 'warn',
     'no-var': 'error',
     'object-shorthand': 'warn',
